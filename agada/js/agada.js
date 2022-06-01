608c748db89399339280251d4239299e074fc2a6
Jake Lerman <jakey.lerman@gmail.com>
2022-06-01 16:53:56 +0100

    finished agada halacha

--- agada/js/agada.js
+++ agada/js/agada.js
@@ -10,57 +10,103 @@ const margin = {top: 30, right: 30, bottom: 200, left: 60},
     .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
   
-  // Parse the Data
-  d3.csv("data/agada_halacha.csv").then( function(data) {
-
-    // event listeners
-    // $("#seder-select").on("change", update)
-    
   // X axis
-  const x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(data.map(d => d.Masechet))
-    .padding(0.2);
+const x = d3.scaleBand()
+  .range([ 0, width ])
+  .padding(0.2);
+
+const xaxis = svg.append("g").attr("transform", `translate(0, ${height})`)
+
+  // Add Y axis
+const y = d3.scaleLinear()
+  .domain([0, 100])
+  .range([ height, 0]);
   svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).tickSize([0]))
+    .call(d3.axisLeft(y))
+    .style("color","#464655ff");
+
+        
+  // Colour Scale
+  const sedarim = ["Kodashim", "Tahorot", "Nashim", "Nezikin","Moed","Zeraim"]
+
+  const masechta_colours = d3.scaleOrdinal().domain(sedarim).range(d3.schemePastel2)
+
+  // Add Legend
+//   const legend = svg.append("g")
+//   .attr("transform", `translate(${width - 10}, ${10})`)
+  
+//   sedarim.forEach((sedarim, i) => {
+// 	const legendRow = legend.append("g")
+// 		.attr("transform", `translate(0, ${i * 20})`)
+
+// 	legendRow.append("rect")
+//     .attr("width", 10)
+//     .attr("height", 10)
+// 		.attr("fill", masechta_colours(sedarim))
+
+// 	legendRow.append("text")
+//     .attr("x", -10)
+//     .attr("y", 10)
+//     .attr("text-anchor", "end")
+//     .style("text-transform", "capitalize")
+// 	.text(sedarim)
+// })
+
+  // Parse the Data
+  d3.csv("data/agada_halacha.csv").then( function(raw_data) {
+    data = raw_data
+
+  // Add X Axis
+    x.domain(raw_data.map(d => d.Masechet))
+    xaxis.call(d3.axisBottom(x))
     .style("color","#464655ff")
     .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
+      .attr("transform", "translate(-10,0)rotate(-35)")
       .style("text-anchor", "end")
       .style("color","#464655ff")
       .attr("font-family", "Calibri")
-      ;
-  
-  // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([ height, 0]);
-  svg.append("g")
-    // .call(d3.axisLeft(y))
-    // .style("color","#464655ff");
-  
-  // Bars
-  svg.selectAll("mybar")
-    .data(data)
-    .join("rect")
-      .attr("x", d => x(d.Masechet))
-      .attr("y", d => y(d.percent_aggada))
-      .attr("width", x.bandwidth())
-      .attr("height", d => height - y(d.percent_aggada))
-      .attr("fill", "#ffadad");
-  
-  // Add a 50% referrence line
-  // const line = d3.line()([[x('Arakhin') + 60, y(40)], [x('Taanit') + 74, y(40)]])
-  // console.log("line",line)
 
-  d3.select("svg")
-      .append("path")
-      .attr("d", line)
-      .attr("stroke", "#464655ff")
-      .style("opacity", 0.5);
-  
+    update(data)
   })
 
-  //TODO: Add update function based on event listner
-  // Add filter to data
+  $("#seder-select")
+    .on("change", () => {
+      update(data)
+    })
+
+  function update(data) {
+
+    // standard transition time for the visualization
+    const t = d3.transition().duration(1000)
+
+    // filter data based on selection
+    const Seder = $("#seder-select").val()
+
+    const filteredData = data.filter(d => {
+      if (Seder === "all") return true
+      else {return d.Seder == Seder}
+        })
+
+// JOIN new data with old elements.
+const bars = svg.selectAll("rect")
+.data(filteredData, d => d.Masechet)
+
+bars.join(
+  function(enter) {return enter.append("rect")
+                      .attr("x", d => x(d.Masechet))
+                      .attr("y", d => y(d.percent_aggada))
+                      .transition(t)
+                      .attr("width", x.bandwidth())
+                      .attr("height", d => height - y(d.percent_aggada))
+                      .attr("fill", d => masechta_colours(d.Seder))
+                      .style('opacity',1)
+                        },
+  function(update) {return update},
+  function(exit) {return exit.transition(t)
+                            .style("opacity",0)
+                            .remove()
+                          }
+)
+  }
+
+// TODO: Working Legend, Tooltip
