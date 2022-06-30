1abb500af90f185b954bcfc90d3a95f542bc62da
Jake Lerman <jakey.lerman@gmail.com>
2022-06-30 11:27:06 +0100

    abaye finished

--- js/personality.js
+++ js/personality.js
@@ -1,5 +1,13 @@
-var width = 1618,
-    height = 1000;
+const MARGIN = { LEFT: 100, RIGHT: 50, TOP: 70, BOTTOM: 50 }
+const WIDTH = 1618 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 1000 - MARGIN.TOP - MARGIN.BOTTOM
+
+const svg = d3.select("#chart-area").append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+const g = svg.append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
  
 var format = d3.formatLocale({
   decimal: ".",
@@ -17,12 +25,48 @@ var nest = d3.nest()
     .key(function(d) { return d.Masechet; })
     .rollup(function(d) { return d3.sum(d, function(d) { return d.adjusted_count; }); });
  
-    console.log("nest",nest)
- 
 var treemap = d3.treemap()
-    .size([width, height])
+    .size([WIDTH, HEIGHT])
     .padding(1)
     .round(true);
+
+function type(d) {
+    d.adjusted_count = +d.adjusted_count;
+    return d;
+  }
+
+// create a tooltip
+var Tooltip = d3.select("#chart-area")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "2px")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+
+// Three function that change the tooltip when user hover / move / leave a cell
+var mouseover = function(d) {
+  Tooltip
+  .style("opacity", 1)
+  d3.select(this)
+  .style("stroke", "black")
+  .style("opacity", 1)
+  }
+var mousemove = function(d) {
+  Tooltip
+  .html("Frequency Rating: " + Math.round(d.value))
+  .style("left", (d3.mouse(this)[0]+10) + "px")
+  .style("top", (d3.mouse(this)[1]) + "px")
+  }
+var mouseleave = function(d) {
+  Tooltip
+  .style("opacity", 0)
+  d3.select(this)
+  .style("stroke", "none")
+  .style("opacity", 0.8)
+  }
  
 d3.csv("data/abaye_treemap_adjusted.csv", type, function(error, data) {
   if (error) throw error;
@@ -32,30 +76,40 @@ d3.csv("data/abaye_treemap_adjusted.csv", type, function(error, data) {
       .sort(function(a, b) { return b.value - a.value; });
  
   treemap(root);
+
  
-  console.log("treemap",treemap(root))
- 
-  var node = d3.select("body")
-    .selectAll(".node")
+  svg
+    .selectAll("rect")
     .data(root.leaves())
-    .enter().append("div")
-      .attr("class", "node")
-      .style("left", function(d) { return d.x0 + "px"; })
-      .style("top", function(d) { return d.y0 + "px"; })
-      .style("width", function(d) { return d.x1 - d.x0 + "px"; })
-      .style("height", function(d) { return d.y1 - d.y0 + "px"; })
-      .style("background", function(d) {  return color(d.parent.data.key); });
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return d.x0 + "px"; })
+      .attr("y", function(d) { return d.y0 + "px"; })
+      .attr("width", function(d) { return d.x1 - d.x0 + "px"; })
+      .attr("height", function(d) { return d.y1 - d.y0 + "px"; })
+      .style("fill", function(d) {  return color(d.parent.data.key); })
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
  
-  node.append("div")
-      .attr("class", "node-label")
-      .text(function(d) { return d.data.key + "\n" + d.parent.data.key; });
+  // node.append("div")
+  //     .attr("class", "node-label")
+  //     .text(function(d) { return d.data.key + "\n" + d.parent.data.key; });
  
-  node.append("div")
-      .attr("class", "node-value")
-      .text(function(d) { return Math.round(d.value); });
+  // node.append("div")
+  //     .attr("class", "node-value")
+  //     .text(function(d) { return Math.round(d.value); });
+
+  // and to add the text labels
+  svg
+    .selectAll("text")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+      .attr("x", function(d){ return d.x0  + 10})    // +10 to adjust position (more right)
+      .attr("y", function(d){ return d.y0  + 20})    // +20 to adjust position (lower)
+      .text(function(d){ return d.data.key})
+      .attr("font-size", "15px")
+      .attr("fill", "white")
+
 });
- 
-function type(d) {
-  d.adjusted_count = +d.adjusted_count;
-  return d;
-}
