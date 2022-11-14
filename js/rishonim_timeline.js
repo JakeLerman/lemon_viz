82a53ef99bbe9410b226fe6b62722d7090aa6fb0
Jake Lerman <jakey.lerman@gmail.com>
2022-11-14 16:43:07 +0000

    working timeline

--- js/rishonim_timeline.js
+++ js/rishonim_timeline.js
@@ -1,17 +1,67 @@
-const MARGIN = { LEFT: 100, RIGHT: 50, TOP: 70, BOTTOM: 50 }
-const WIDTH = 1618 - MARGIN.LEFT - MARGIN.RIGHT
-const HEIGHT = 1000 - MARGIN.TOP - MARGIN.BOTTOM
+const MARGIN = { LEFT: 200, RIGHT: 50, TOP: 70, BOTTOM: 50 }
+const WIDTH = 2000 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 1200 - MARGIN.TOP - MARGIN.BOTTOM
 
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+.append("g")
+.attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
 
-const g = svg.append("g")
-  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+// Lloyds Colour Scheme
+const colors = d3.schemePastel1
  
-
- 
-d3.csv("data/Rishonim.xlsx").then(data => {
-
+d3.csv("data/Rishonim.csv").then(data => {
   console.log(data)
-});
+
+  // Data Pre-Processing
+data.forEach(element => {
+    element['birth'] = Number(element['birth'])
+    element['death'] = Number(element['death'])
+    return data
+  })
+
+ 
+  // Sort data
+  data.sort(function(a,b) { return d3.descending(a['birth'], b['birth']) || d3.descending(a['death'], b['death']) })
+ 
+  // X axis
+  const xAxis = d3
+    .scaleLinear()
+    .domain([
+      d3.min(data, (d) => d["birth"]),
+      d3.max(data, (d) => d["death"]),
+    ])
+    .range([0, WIDTH]);
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${HEIGHT})`)
+    .call(d3.axisBottom(xAxis));
+ 
+  // Y Axis
+  const yAxis = d3
+    .scaleBand()
+    .domain(data.map((d) => d["name"]))
+    .range([HEIGHT, 0])
+    .paddingInner(0.3);
+  svg.append("g").call(d3.axisLeft(yAxis))
+ 
+  svg.selectAll("bars")
+  .data(data)
+  .join("rect")
+    .attr("x", d => xAxis(d['birth']))
+    .attr("y", d => yAxis(d['name']))
+    .attr("width", d => xAxis(d['death']) - xAxis(d['birth']))
+    .attr("height", yAxis.bandwidth())
+    .attr("fill", "#338770")
+    .append("title")
+    .text(d => d.name + ': ' + d.birth + ' - ' + d.death)
+    // .attr("opacity",d=> cScale(d.Perc_Left))
+ 
+  // svg.selectAll("labels")
+  //   .data(data)
+  //   .join("text")
+  //   .attr("x", d => xAxis(d['birth']))
+  //   .attr("y", d => yAxis(d['name'])+yAxis.bandwidth())
+  //   .text(d => d.name)
+  })
