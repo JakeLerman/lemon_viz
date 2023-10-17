58ef556d58e217ce72c83579793c84c475226636
Jake Lerman <WinUser@MacBook-Pro.local>
2023-10-17 10:00:47 +0100

    prettier and eslint

--- js/achronim_timeline.js
+++ js/achronim_timeline.js
@@ -1,144 +1,164 @@
 // Global Variables
-const MARGIN = { LEFT: 50, RIGHT: 200, TOP: 110, BOTTOM: 50 }
-const WIDTH = 1200 - MARGIN.LEFT - MARGIN.RIGHT
-const HEIGHT = 2000 - MARGIN.TOP - MARGIN.BOTTOM
+const MARGIN = {
+  LEFT: 50, RIGHT: 200, TOP: 110, BOTTOM: 50,
+};
+const WIDTH = 1200 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 2000 - MARGIN.TOP - MARGIN.BOTTOM;
 
-const svg = d3.select("#chart-area").append("svg")
-  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
-  .attr("viewBox", [0, 0, WIDTH + MARGIN.LEFT + MARGIN.RIGHT, HEIGHT + MARGIN.TOP + MARGIN.BOTTOM])
-  .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
-  .append("g")
-  .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
+const svg = d3
+  .select('#chart-area')
+  .append('svg')
+  .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+  .attr('viewBox', [
+    0,
+    0,
+    WIDTH + MARGIN.LEFT + MARGIN.RIGHT,
+    HEIGHT + MARGIN.TOP + MARGIN.BOTTOM,
+  ])
+  .attr('style', 'max-width: 100%; height: auto; height: intrinsic;')
+  .append('g')
+  .attr('transform', `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
 
-let data
- 
+let data;
+
 // Scales/Axis
-const xAxis = d3
-.scaleLinear()
-.range([0, WIDTH]);
-const yAxis = d3
-    .scaleBand()
-    .range([HEIGHT, 0])
-    .paddingInner(0.3);
-const colors = d3.schemePastel1
-const cScale = d3.scaleOrdinal()
-    .range(colors)
+const xAxis = d3.scaleLinear().range([0, WIDTH]);
+const yAxis = d3.scaleBand().range([HEIGHT, 0]).paddingInner(0.3);
+const colors = d3.schemePastel1;
+const cScale = d3.scaleOrdinal().range(colors);
 
-  $("#reset-button")
-  .on("click", () => {
-    initVis()
-  })
+$('#reset-button').on('click', () => {
+  initVis();
+});
 
 // Load Data
-d3.tsv("data/bios_sefaria_preprocessed.tsv").then(rawdata => {
-    // Data Pre-Processing
-    rawdata.forEach(element => {
-      element['Birth Year '] = Number(element['Birth Year '])
-      element['Death Year'] = Number(element['Death Year'])
-      return rawdata
-    })
-    // Sort data
-    data = rawdata.sort(function(a,b) { return d3.descending(a['Birth Year '], b['Birth Year ']) || d3.descending(a['Death Year'], b['Death Year']) })
+d3.tsv('data/bios_sefaria_preprocessed.tsv').then((rawdata) => {
+  // Data Pre-Processing
+  rawdata.forEach((element) => {
+    element['Birth Year '] = Number(element['Birth Year ']);
+    element['Death Year'] = Number(element['Death Year']);
+    return rawdata;
+  });
+  // Sort data
+  data = rawdata.sort((a, b) => (
+    d3.descending(a['Birth Year '], b['Birth Year '])
+      || d3.descending(a['Death Year'], b['Death Year'])
+  ));
 
-    console.log(data)
+  console.log(data);
 
-    $("#search").autocomplete({source: data.map(d=> d['Primary English Name']),select: function (event, ui) {
+  $('#search').autocomplete({
+    source: data.map((d) => d['Primary English Name']),
+    select(event, ui) {
       // var label = ui.item.label;
-      var value = ui.item.value;
-      update(value)
-    }})
-    
-    // init Viz
-    initVis()
-})
+      const { value } = ui.item;
+      update(value);
+    },
+  });
 
-function initVis(){
-// clear
-svg.selectAll("*").remove();
+  // init Viz
+  initVis();
+});
+
+function initVis() {
+  // clear
+  svg.selectAll('*').remove();
 
   // X axis
-  xAxis
-  .domain([
-    d3.min(data, (d) => d["Birth Year "]),
-    d3.max(data, (d) => d["Death Year"]),
+  xAxis.domain([
+    d3.min(data, (d) => d['Birth Year ']),
+    d3.max(data, (d) => d['Death Year']),
   ]);
-svg
-  .append("g")
-  .attr("transform", `translate(0,-10)`)
-  .call(d3.axisTop(xAxis).tickFormat(d3.format("d")));
+  svg
+    .append('g')
+    .attr('transform', 'translate(0,-10)')
+    .call(d3.axisTop(xAxis).tickFormat(d3.format('d')));
 
-// Y Axis
-yAxis.domain(data.map((d) => d["Primary English Name"]))
-// svg.append("g").call(d3.axisLeft(yAxis))
+  // Y Axis
+  yAxis.domain(data.map((d) => d['Primary English Name']));
+  // svg.append("g").call(d3.axisLeft(yAxis))
 
-const locations = [...new Set(data.map(d=>d.location))]
+  const locations = [...new Set(data.map((d) => d.location))];
 
-// Color
-cScale
-  .domain(locations)
+  // Color
+  cScale.domain(locations);
 
-// Bars
-svg.selectAll(".bars")
-  .data(data)
-  .join("a")
-  .attr("class","bars")
-  .attr("href", d => d['English Wikipedia Link'])
-  .attr("target", d => d['English Wikipedia Link'])
-  .append("rect")
-    .attr("x", d => xAxis(d['Birth Year ']))
-    .attr("y", d => yAxis(d['Primary English Name']))
-    .attr("width", d => xAxis(d['Death Year']) - xAxis(d['Birth Year ']))
-    .attr("height", yAxis.bandwidth())
-    .attr("fill", cScale("test")) //d=> cScale(d.location)
-    .append("title")
-    // .text(d => d.description)
-    .text(d => d['Primary English Name'] + ': ' + d['Birth Year '] + ' - ' + d['Death Year'] + '; ' + d['English Biography'])
-    // .attr("opacity",d=> cScale(d.Perc_Left))
-
-//Labels  
-svg.selectAll(".labels")
+  // Bars
+  svg
+    .selectAll('.bars')
     .data(data)
-    .join("text")
-    .attr("class","labels")
-    .attr("x", d => xAxis(d['Death Year']))
-    .attr("y", d => yAxis(d['Primary English Name'])+yAxis.bandwidth())
-    .text(d => d['Primary English Name'])
-    // .style("fill", d => d.location)
-    .style("font-size",9)
-    .style("font-weight","bold")
+    .join('a')
+    .attr('class', 'bars')
+    .attr('href', (d) => d['English Wikipedia Link'])
+    .attr('target', (d) => d['English Wikipedia Link'])
+    .append('rect')
+    .attr('x', (d) => xAxis(d['Birth Year ']))
+    .attr('y', (d) => yAxis(d['Primary English Name']))
+    .attr('width', (d) => xAxis(d['Death Year']) - xAxis(d['Birth Year ']))
+    .attr('height', yAxis.bandwidth())
+    .attr('fill', cScale('test')) // d=> cScale(d.location)
+    .append('title')
+    // .text(d => d.description)
+    .text(
+      (d) => `${d['Primary English Name']
+      }: ${
+        d['Birth Year ']
+      } - ${
+        d['Death Year']
+      }; ${
+        d['English Biography']}`,
+    );
+  // .attr("opacity",d=> cScale(d.Perc_Left))
 
-    // Legend
+  // Labels
+  svg
+    .selectAll('.labels')
+    .data(data)
+    .join('text')
+    .attr('class', 'labels')
+    .attr('x', (d) => xAxis(d['Death Year']))
+    .attr('y', (d) => yAxis(d['Primary English Name']) + yAxis.bandwidth())
+    .text((d) => d['Primary English Name'])
+    // .style("fill", d => d.location)
+    .style('font-size', 9)
+    .style('font-weight', 'bold');
+
+  // Legend
   //   svg.append("g")
   // .attr("class", "legend")
   // .attr("transform", `translate(${WIDTH},10)`);
   // const legend = d3.legendColor().scale(cScale)
   // svg.select(".legend")
   // .call(legend);
-  }
-
+}
 
 function update(name) {
-  const filteredData = data.filter(row => row['Primary English Name'] == name)
-  console.log(filteredData)
+  const filteredData = data.filter(
+    (row) => row['Primary English Name'] == name,
+  );
+  console.log(filteredData);
 
-  svg.selectAll(".bars")
-    .data(filteredData, d => d['Primary English Name'])
+  svg
+    .selectAll('.bars')
+    .data(filteredData, (d) => d['Primary English Name'])
     .join(
-      function(enter) {
-        console.log("enter",enter)
+      (enter) => {
+        console.log('enter', enter);
       },
-      function(update) {
-        update.selectAll("rect").attr("fill","#ff6961")
-        console.log("update", update)
+      (update) => {
+        update.selectAll('rect').attr('fill', '#ff6961');
+        console.log('update', update);
       },
-      function(exit) {
-        console.log("exit",exit)
-        return exit.selectAll("rect").style('opacity', 0.5).attr("fill",cScale("test"));
-      }
-    )
-
-  }
+      (exit) => {
+        console.log('exit', exit);
+        return exit
+          .selectAll('rect')
+          .style('opacity', 0.5)
+          .attr('fill', cScale('test'));
+      },
+    );
+}
 
 // TODO:
 // Work out how to display both Long name and colloquial name/Hebrew name?
--- js/agada.js
+++ js/agada.js
@@ -1,239 +1,245 @@
-var margin = ({top: 10, right: 60, bottom: 50, left: 60})
-var height = 300, width=600;
-var font = 'Georgia'
-  
-  // append the svg object to the body of the page
-  var svg = d3.select(".chart")
-    .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-  
-  // X axis
-const x = d3.scaleBand()
-  .range([ 0, width - margin.left - margin.right])
-  .padding(0.2);
-    // domain added with data
+const margin = {
+  top: 10, right: 60, bottom: 50, left: 60,
+};
+const height = 300;
+const width = 600;
+const font = 'Georgia';
 
-  // Add Y axis
-const y = d3.scaleLinear()
+// append the svg object to the body of the page
+const svg = d3
+  .select('.chart')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+// X axis
+const x = d3
+  .scaleBand()
+  .range([0, width - margin.left - margin.right])
+  .padding(0.2);
+// domain added with data
+
+// Add Y axis
+const y = d3
+  .scaleLinear()
   .domain([0, 100])
   .range([height - margin.top - margin.bottom, 0]);
-        
-  // Colour Scale
-  const sedarim = ["Kodashim", "Tahorot", "Nashim", "Nezikin","Moed","Zeraim", 0, 1]
-  const masechta_colours = d3.scaleOrdinal().domain(sedarim).range(d3.schemePastel2)
 
-  // standard transition time for the visualization
-  const t = d3.transition().duration(1000)
+// Colour Scale
+const sedarim = [
+  'Kodashim',
+  'Tahorot',
+  'Nashim',
+  'Nezikin',
+  'Moed',
+  'Zeraim',
+  0,
+  1,
+];
+const masechta_colours = d3
+  .scaleOrdinal()
+  .domain(sedarim)
+  .range(d3.schemePastel2);
 
-  // load data
-  d3.csv("data/agada_halacha.csv").then( function(raw_data) {
-    data = raw_data})
-  
+// standard transition time for the visualization
+const t = d3.transition().duration(1000);
+
+// load data
+d3.csv('data/agada_halacha.csv').then((raw_data) => {
+  data = raw_data;
+});
+
 // ------------------------ Pie Chart------------------------------
 
-
 function buildPieChart() {
+  const radius = Math.min(width, height) / 2.1;
 
-  const radius = Math.min(width, height) / 2.1
+  const pie = d3.pie();
 
-  const pie = d3.pie()
-
-  const pieData = pie([1610388,255690])
+  const pieData = pie([1610388, 255690]);
 
   g = svg
-  .append("g")
-    .attr("transform", `translate(${width/2},${height/2})`);
+    .append('g')
+    .attr('transform', `translate(${width / 2},${height / 2})`);
 
-  g.selectAll("pie")
-      .data(pieData)
-      .join('path')
-      .attr('d', d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius))
-      .attr('stroke', 'white')
-      .style('fill', d => masechta_colours(d.index))
-      .style("stroke-width", "2px")
-      .style("opacity", 0)
-      .transition(d3.transition().duration(700))
-      .style("opacity", 0.7)
-
+  g.selectAll('pie')
+    .data(pieData)
+    .join('path')
+    .attr('d', d3.arc().innerRadius(0).outerRadius(radius))
+    .attr('stroke', 'white')
+    .style('fill', (d) => masechta_colours(d.index))
+    .style('stroke-width', '2px')
+    .style('opacity', 0)
+    .transition(d3.transition().duration(700))
+    .style('opacity', 0.7);
 }
 
 // ------------------------ Bar Chart------------------------------
 
-  // Parse the Data
-  function loadInitialGraph () {
+// Parse the Data
+function loadInitialGraph() {
   // d3.csv("data/agada_halacha.csv").then( function(raw_data) {
   //   data = raw_data
   g = svg
-  .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    x.domain(data.map(d => d.Masechet))
-    g.append("g").attr("transform", `translate(0, ${height - margin.top - margin.bottom})`)
+  x.domain(data.map((d) => d.Masechet));
+  g.append('g')
+    .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
     .call(d3.axisBottom(x))
-    .style("color","#464655ff")
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-35)")
-      .style("text-anchor", "end")
-      .style("color","#464655ff")
-      .attr("font-family", "Calibri")
+    .style('color', '#464655ff')
+    .selectAll('text')
+    .attr('transform', 'translate(-10,0)rotate(-35)')
+    .style('text-anchor', 'end')
+    .style('color', '#464655ff')
+    .attr('font-family', 'Calibri');
 
-      g.append("g")
-      .call(d3.axisLeft(y))
-      .style("color","#464655ff");
+  g.append('g').call(d3.axisLeft(y)).style('color', '#464655ff');
 
-    update(data, "all")
+  update(data, 'all');
   // })
 }
 
-  function update(data, Seder = "all") {
+function update(data, Seder = 'all') {
+  // filter data based on selection
 
-    // filter data based on selection
+  const filteredData = data.filter((d) => {
+    if (Seder === 'all') return true;
 
-    const filteredData = data.filter(d => {
-      if (Seder === "all") return true
-      else {return d.Seder == Seder}
-        })
+    return d.Seder == Seder;
+  });
 
-    // JOIN new data with old elements.
-    const bars = g.selectAll("rect")
-    .data(filteredData, d => d.Masechet)
+  // JOIN new data with old elements.
+  const bars = g.selectAll('rect').data(filteredData, (d) => d.Masechet);
 
-    bars.join(
-      function(enter) {return enter.append("rect")
-                          .attr("x", d => x(d.Masechet))
-                          .attr("y", d => y(d.percent_aggada))
-                          .transition(d3.transition().duration(500))
-                          .attr("width", x.bandwidth())
-                          .attr("height", d => height - margin.top - margin.bottom - y(d.percent_aggada))
-                          .attr("fill", d => masechta_colours(d.Seder))
-                          .style('opacity',1)
-                            },
-      function(update) {return update},
-      function(exit) {return exit.transition(d3.transition().duration(500))
-                                .style("opacity",0)
-                                .remove()
-                              }
-    )
-  }
+  bars.join(
+    (enter) => enter
+      .append('rect')
+      .attr('x', (d) => x(d.Masechet))
+      .attr('y', (d) => y(d.percent_aggada))
+      .transition(d3.transition().duration(500))
+      .attr('width', x.bandwidth())
+      .attr(
+        'height',
+        (d) => height - margin.top - margin.bottom - y(d.percent_aggada),
+      )
+      .attr('fill', (d) => masechta_colours(d.Seder))
+      .style('opacity', 1),
+    (update) => update,
+    (exit) => exit
+      .transition(d3.transition().duration(500))
+      .style('opacity', 0)
+      .remove(),
+  );
+}
 
-  function clear() {
-    svg.selectAll("g")
-      .transition(1000)
-      .style("opacity", 0)
-      .remove();
-  }
+function clear() {
+  svg.selectAll('g').transition(1000).style('opacity', 0).remove();
+}
 
-//------------------------Scrollama----------------------------------
-//// using d3 for convenience, and storing a selected elements
-var container = d3.select('#scroll');
-var graphic = container.select('.scroll__graphic');
-var chart = graphic.select('.chart');
-var text = container.select('.scroll__text');
-var step = text.selectAll('.step');
+// ------------------------Scrollama----------------------------------
+/// / using d3 for convenience, and storing a selected elements
+const container = d3.select('#scroll');
+const graphic = container.select('.scroll__graphic');
+const chart = graphic.select('.chart');
+const text = container.select('.scroll__text');
+const step = text.selectAll('.step');
 
 // initialize the scrollama
-var scroller = scrollama();
+const scroller = scrollama();
 
 // resize function to set dimensions on load and on page resize
-function handleResize() { 
-// 1. update height of step elements for breathing room between steps
-	var stepHeight = Math.floor(window.innerHeight * 0.75);
-	step.style('height', stepHeight + 'px');
+function handleResize() {
+  // 1. update height of step elements for breathing room between steps
+  const stepHeight = Math.floor(window.innerHeight * 0.75);
+  step.style('height', `${stepHeight}px`);
 
-	// 2. update height of graphic element
-	var bodyWidth = d3.select('body').node().offsetWidth;
+  // 2. update height of graphic element
+  const bodyWidth = d3.select('body').node().offsetWidth;
 
-	graphic
-		.style('height', window.innerHeight + 'px');
+  graphic.style('height', `${window.innerHeight}px`);
 
-	// 3. update width of chart by subtracting from text width
-	var chartMargin = 32;
-	var textWidth = text.node().offsetWidth;
-	var chartWidth = graphic.node().offsetWidth - textWidth - chartMargin;
-	// make the height 1/2 of viewport
-	var chartHeight = Math.floor(window.innerHeight / 2);
+  // 3. update width of chart by subtracting from text width
+  const chartMargin = 32;
+  const textWidth = text.node().offsetWidth;
+  const chartWidth = graphic.node().offsetWidth - textWidth - chartMargin;
+  // make the height 1/2 of viewport
+  const chartHeight = Math.floor(window.innerHeight / 2);
 
-	chart
-		.style('width', chartWidth + 'px')
-		.style('height', chartHeight + 'px');
+  chart.style('width', `${chartWidth}px`).style('height', `${chartHeight}px`);
 
-	// 4. tell scrollama to update new element dimensions
-	scroller.resize();
+  // 4. tell scrollama to update new element dimensions
+  scroller.resize();
 }
 
 // scrollama event handlers
 
 function handleStepEnter(response) {
-	// response = { element, direction, index }
-	// fade in current step
-	step.classed('is-active', function (d, i) {
-		return i === response.index;
-	})
+  // response = { element, direction, index }
+  // fade in current step
+  step.classed('is-active', (d, i) => i === response.index);
 
-	// update graphic based on step here
-	var stepData = parseFloat(response.element.getAttribute('data-step'));
-	console.log(stepData)
-              // Scrolling Down
-  if (stepData === 0 && response.direction == "down") {
-    buildPieChart()
+  // update graphic based on step here
+  const stepData = parseFloat(response.element.getAttribute('data-step'));
+  console.log(stepData);
+  // Scrolling Down
+  if (stepData === 0 && response.direction == 'down') {
+    buildPieChart();
   }
 
-  if (stepData === 1 && response.direction == "down") {
-    clear()
-    loadInitialGraph()
-    update(data, "all")
+  if (stepData === 1 && response.direction == 'down') {
+    clear();
+    loadInitialGraph();
+    update(data, 'all');
   }
 
   if (stepData === 2) {
-    update(data, "Seder Kodashim")
+    update(data, 'Seder Kodashim');
   }
 
   if (stepData === 3) {
-    update(data, "Seder Taharos")
+    update(data, 'Seder Taharos');
   }
 
   if (stepData === 4) {
-    update(data, "Seder Nashim")
+    update(data, 'Seder Nashim');
   }
 
   if (stepData === 5) {
-    update(data, "Seder Nezikin")
+    update(data, 'Seder Nezikin');
   }
 
   if (stepData === 6) {
-    update(data, "Seder Moed")
+    update(data, 'Seder Moed');
   }
 
-  if (stepData === 7 && response.direction == "down") {
-    update(data, "Seder Zeraim")
+  if (stepData === 7 && response.direction == 'down') {
+    update(data, 'Seder Zeraim');
   }
 
-  if (stepData === 8 && response.direction == "down") {
-    clear()
+  if (stepData === 8 && response.direction == 'down') {
+    clear();
   }
-          // Scrolling Up
+  // Scrolling Up
 
-  if (stepData === 7 && response.direction == "up") {
-    loadInitialGraph()
-    update(data, "Seder Zeraim")
-    }
-  
-  if (stepData === 1 && response.direction == "up") {
-    update(data, "all")
-    }
-  
-  if (stepData === 0 && response.direction == "up") {
-    clear()
-    buildPieChart()
-    }
-  
+  if (stepData === 7 && response.direction == 'up') {
+    loadInitialGraph();
+    update(data, 'Seder Zeraim');
+  }
+
+  if (stepData === 1 && response.direction == 'up') {
+    update(data, 'all');
+  }
+
+  if (stepData === 0 && response.direction == 'up') {
+    clear();
+    buildPieChart();
+  }
+
   if (stepData === -1) {
-    clear()
-    }
-	
+    clear();
+  }
 }
 
 // function handleContainerEnter(response) {
@@ -254,26 +260,26 @@ function handleStepEnter(response) {
 
 // kick-off code to run once on load
 function init() {
-	// 1. call a resize on load to update width/height/position of elements
-	handleResize();
+  // 1. call a resize on load to update width/height/position of elements
+  handleResize();
 
-	// 2. setup the scrollama instance
-	// 3. bind scrollama event handlers (this can be chained like below)
-	scroller
-		.setup({
-			// container: '#scroll', // our outermost scrollytelling element
-			// graphic: '.scroll__graphic', // the graphic
-			// text: '.scroll__text', // the step container
-			step: '.scroll__text .step', // the step elements
-			offset: 0.7, // set the trigger to be 1/2 way down screen
-			debug: false, // display the trigger offset for testing
-		})
-		.onStepEnter(handleStepEnter)
-		//.onContainerEnter(handleContainerEnter)
-		//.onContainerExit(handleContainerExit);
+  // 2. setup the scrollama instance
+  // 3. bind scrollama event handlers (this can be chained like below)
+  scroller
+    .setup({
+      // container: '#scroll', // our outermost scrollytelling element
+      // graphic: '.scroll__graphic', // the graphic
+      // text: '.scroll__text', // the step container
+      step: '.scroll__text .step', // the step elements
+      offset: 0.7, // set the trigger to be 1/2 way down screen
+      debug: false, // display the trigger offset for testing
+    })
+    .onStepEnter(handleStepEnter);
+  // .onContainerEnter(handleContainerEnter)
+  // .onContainerExit(handleContainerExit);
 
-	// setup resize event
-	window.addEventListener('resize', handleResize);
+  // setup resize event
+  window.addEventListener('resize', handleResize);
 }
 
-init()
+init();
